44fb559527cb3b587bda9dec2626597fc1c86aa5
Idrak olayinka <idrakolayinka23@gmail.com>
2026-09-16 01:02:25 +1030

    Add files via upload

---
+++ basketball.js
@@ -0,0 +1,28 @@
+let sum = document.getElementById("add-el")
+let sum1 = document.getElementById("add1-el")
+ let num = 0
+ let num1 = 0
+function add (){
+    num += 1
+ sum.textContent = num
+}
+function add2 (){
+    num += 2
+ sum.textContent = num
+}
+function add3 (){
+    num += 3
+ sum.textContent = num
+}
+function add1G (){
+    num1 += 1
+ sum1.textContent = num1
+}
+function add2G (){
+    num1 += 2
+ sum1.textContent = num1
+}
+function add3G (){
+    num1 += 3
+ sum1.textContent = num1
+}
